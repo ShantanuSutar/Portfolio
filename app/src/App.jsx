9bab20a35e8d2f0e6310e07b177d1e9cf9bd3e3b
Shantanu Sutar <shantanusutar17@gmail.com>
2023-09-18 12:53:43 +0530

    initial setup

---
+++ app/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className=" h-screen text-3xl flex items-center justify-center body-font font-poppins">
+      <h1 className="">Shantanu</h1>
+    </div>
+  );
+}
+
+export default App;
