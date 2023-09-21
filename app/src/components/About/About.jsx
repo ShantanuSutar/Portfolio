1bc87e42b7924c87d53fc3548d0f99574ece8ede
Shantanu Sutar <shantanusutar17@gmail.com>
2023-09-21 20:53:30 +0530

    About - Dark Mode

--- app/src/components/About/About.jsx
+++ app/src/components/About/About.jsx
@@ -4,7 +4,9 @@ const About = () => {
   return (
     <div className=" min-h-[90%] w-full  pt-5 md:pt-20 " id="about">
       <div className=" mx-auto flex flex-col items-center gap-1 ">
-        <h2 className=" text-2xl font-semibold md:text-4xl">About Me</h2>
+        <h2 className=" text-2xl font-semibold text-slate-900 dark:text-white md:text-4xl">
+          About Me
+        </h2>
         <span className=" text-slate-500">My Introduction</span>
       </div>
       <div className=" mx-8 flex flex-col md:m-10  md:grid md:grid-cols-2 md:items-center md:gap-x-10">
@@ -15,7 +17,7 @@ const About = () => {
         />
         <div className=" flex flex-col items-center gap-10">
           <AboutInfo />
-          <p className=" text-center text-slate-600">
+          <p className=" da text-center text-slate-600 dark:text-slate-300">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse odit
             nobis voluptate repellat molestias optio, ipsum architecto facilis
             perferendis non laboriosam Lorem, ipsum dolor sit amet consectetur
@@ -23,7 +25,7 @@ const About = () => {
           </p>
           <a
             href=""
-            className=" text-md flex max-w-[10rem] items-center justify-center gap-2 rounded-xl bg-black px-6 py-4 font-medium text-slate-200 transition duration-75 ease-in-out hover:opacity-90"
+            className=" text-md flex max-w-[10rem] items-center justify-center gap-2 rounded-xl bg-slate-950 px-6 py-4 font-medium text-slate-200 transition duration-75 ease-in-out hover:opacity-90 dark:bg-slate-200 dark:text-slate-900"
           >
             Resume
             <CgFileDocument className=" text-xl" />
--- app/src/components/About/AboutInfo.jsx
+++ app/src/components/About/AboutInfo.jsx
@@ -4,21 +4,27 @@ import { BsFillJournalBookmarkFill } from "react-icons/bs";
 
 const AboutInfo = () => {
   return (
-    <div className=" grid grid-cols-3 gap-4 text-center sm:flex sm:justify-around">
+    <div className=" grid grid-cols-3 gap-4 text-center text-slate-900 dark:text-slate-200 sm:flex sm:justify-around">
       <div className=" flex cursor-default flex-col items-center justify-around gap-1 transition-all ease-linear hover:shadow-2xl">
         <FaLaptopCode className=" text-xl" />
         <h3 className=" font-medium sm:text-lg">Open Source</h3>
-        <span className=" text-xs text-slate-600">Active Contributions</span>
+        <span className=" text-xs text-slate-600 dark:text-slate-400">
+          Active Contributions
+        </span>
       </div>
       <div className="flex cursor-default flex-col items-center gap-1 transition-all ease-linear hover:shadow-2xl">
         <HiOutlineClipboardDocumentList className=" text-xl" />
         <h3 className=" font-medium sm:text-lg">Completed</h3>
-        <span className=" text-xs text-slate-600">50 + Projects</span>
+        <span className=" text-xs text-slate-600 dark:text-slate-400">
+          50 + Projects
+        </span>
       </div>
       <div className="flex  cursor-default flex-col items-center gap-1 transition-all ease-linear hover:shadow-2xl">
         <BsFillJournalBookmarkFill className=" text-xl" />
         <h3 className=" font-medium sm:text-lg">Studies</h3>
-        <span className=" text-xs text-slate-600">Pursuing C.S.</span>
+        <span className=" text-xs text-slate-600 dark:text-slate-400">
+          Pursuing C.S.
+        </span>
       </div>
     </div>
   );
